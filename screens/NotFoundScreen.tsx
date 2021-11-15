aec5faf35ed3ce9e449247f186be96a9c705db6c
Darshan Chobarkar <dchobarkar@gmail.com>
2021-11-15 16:39:09 +0530

    NotFoundScreen code added

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,8 +1,40 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { NotFoundScreenProps } from "../utils/types";
 
-function NotFoundScreen() {
-  return <View></View>;
+function NotFoundScreen({ navigation }: NotFoundScreenProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This screen doesn't exist.</Text>
+      <TouchableOpacity
+        onPress={() => navigation.replace("Search")}
+        style={styles.link}
+      >
+        <Text style={styles.linkText}>Go to home screen!</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: "#2e78b7",
+  },
+});
 
 export default NotFoundScreen;
